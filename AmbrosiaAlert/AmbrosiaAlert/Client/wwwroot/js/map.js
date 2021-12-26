929636288563b0025d8244599317b9a0cbe593c2
Daniel Horț <dnielhort@gmail.com>
2021-12-26 21:45:38 +0200

    center map to user position

--- AmbrosiaAlert/AmbrosiaAlert/Client/wwwroot/js/map.js
+++ AmbrosiaAlert/AmbrosiaAlert/Client/wwwroot/js/map.js
@@ -1,44 +1,13 @@
-﻿var options = {
+﻿var defaultOptions = {
     enableHighAccuracy: true,
     timeout: 1000,
     maximumAge: 0
 };
 
-/*function success(pos) {
-    var crd = pos.coords;
-
-    console.log(pos);
-    console.log('Your current position is:');
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
-
-    map.flyTo({
-        center: [crd.latitude, crd.longitude],
-        zoom: 16,
-        duration: 1
-    });
-}
-
-function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-}*/
-
-var result;
-
-let mapPositionToResult = (position, result) => {
-    result.position = [position.coords.longitude, position.coords.latitude];
-};
-
-let mapErrorToResult = (error, result) => {
-    result.error = {
-        code: error.code,
-        message: error.message
-    }
-};
-
 let getCurrentPosition = async (options) => {
-    result = { position: null, error: null };
+    if (!options)
+        options = defaultOptions;
+    var result = { position: null, error: null };
     var getCurrentPositionPromise = new Promise((resolve, reject) => {
         if (!navigator.geolocation) {
             reject({ code: 0, message: 'This device does not support geolocation.' });
@@ -47,16 +16,19 @@ let getCurrentPosition = async (options) => {
         }
     });
     await getCurrentPositionPromise.then(
-        (position) => { mapPositionToResult(position, result) }
+        (position) => result.position = [position.coords.longitude, position.coords.latitude]
     ).catch(
-        (error) => { mapErrorToResult(error, result) }
+        (error) => result.error = {
+            code: error.code,
+            message: error.message
+        }
     );
     return result;
 };
 
 window.createMap = async (id /*string*/) => {
 
-    let position = await getCurrentPosition(options);
+    let position = await getCurrentPosition();
 
     mapboxgl.accessToken = "pk.eyJ1IjoiZGFuaWVsMWhvcnQiLCJhIjoiY2t4a3dpNml2MHlhdTJxcWtnZjVjMWQ1MyJ9.5rn3CXbkf7xE5dfI4tmmww";
     let map = new mapboxgl.Map({
