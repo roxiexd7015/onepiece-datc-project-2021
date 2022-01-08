350a0f0d0f3ccd5c220f4ee26a99023fa690b9c3
Daniel Horț <dnielhort@gmail.com>
2022-01-08 22:51:22 +0200

    add authentication

---
+++ AmbrosiaAlert/AmbrosiaAlert/Client/Extra/LocalStorageAuthStateProvider.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace AmbrosiaAlert.Client.Extra
+{
+    public class LocalStorageAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService localStorage;
+
+        public LocalStorageAuthStateProvider(ILocalStorageService localStorage)
+        {
+            this.localStorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await localStorage.GetItem("authtoken");
+            var handler = new JwtSecurityTokenHandler();
+
+            var jwt = string.IsNullOrEmpty(token) switch
+            {
+                true => null,
+                _ => handler.ReadJwtToken(token)
+            };
+
+            // TODO: check expiration
+            var identity = jwt switch
+            {
+                JwtSecurityToken s => new ClaimsIdentity(handler.ReadJwtToken(token).Claims, "My custom auth service"),
+                _ => new ClaimsIdentity()
+            };
+            var state = new AuthenticationState(new ClaimsPrincipal(identity));
+            return state;
+        }
+    }
+}
---
+++ AmbrosiaAlert/AmbrosiaAlert/Client/Extra/LocalStorageService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.JSInterop;
+
+namespace AmbrosiaAlert.Client.Extra
+{
+    public interface ILocalStorageService
+    {
+        Task<string> GetItem(string key);
+        Task SetItem(string key, string value);
+        Task RemoveItem(string key);
+    }
+
+    public class LocalStorageService : ILocalStorageService
+    {
+        private readonly IJSRuntime js;
+
+        public LocalStorageService(IJSRuntime js)
+        {
+            this.js = js;
+        }
+
+        public async Task<string> GetItem(string key)
+        {
+            var value = await js.InvokeAsync<string>("localStorage.getItem", key);
+            return value;
+        }
+
+        public async Task SetItem(string key, string value)
+        {
+            await js.InvokeVoidAsync("localStorage.setItem", key, value);
+        }
+
+        public async Task RemoveItem(string key)
+        {
+            await js.InvokeVoidAsync("localStorage.removeItem", key);
+        }
+    }
+}
